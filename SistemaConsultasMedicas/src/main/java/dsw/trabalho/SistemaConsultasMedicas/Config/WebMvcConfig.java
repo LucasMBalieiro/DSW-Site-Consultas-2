c87c9f2f9f01dda7b1d82f9b9f11c34c3c9603ed
Ast <vitor.orsin@estudante.ufscar.br>
2023-09-04 15:00:16 -0300

    Better language change

--- SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Config/WebMvcConfig.java
+++ SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Config/WebMvcConfig.java
@@ -10,9 +10,11 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -32,10 +34,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("pt", "BR"));
-        return slr;
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(new Locale("pt", "BR"));
+        return sessionLocaleResolver;
     }
 
     @Bean
